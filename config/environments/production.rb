51ff55da1cb6fd410ce6b0b07513006828420f76
Wayne Brissette <wbrisett@att.net>
2014-01-14 15:41:57 -0600

    add configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,11 @@ LearnRails::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  
+  config.action_mailer.smtp_settings = {
+  :address   => "smtp.mandrillapp.com",
+  :port      => 587,
+  :user_name => ENV["d"],
+  :password  => ENV["f"]
+}
 end
